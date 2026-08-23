cbcf794f5fcfa21a45e13bf81b58be82a6a73ff7
fryh094-web <fryh094@gmail.com>
2026-08-24 00:12:24 +0100

    Create health.js

---
+++ routes/health.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    status: "ok",
+    service: "bank-demo-server"
+  });
+});
+
+module.exports = router;
